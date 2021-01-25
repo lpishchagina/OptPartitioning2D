ac48430bbd65de17d0fe9db2895c5ca33ba59913
lpishchagina <77454431+lpishchagina@users.noreply.github.com>
2021-01-25 17:20:32 +0100

    update

--- simulation/SimpleTest2D.R
+++ simulation/SimpleTest2D.R
@@ -1,8 +1,9 @@
-devtools::install_github("lpishchagina/OptPartitioning2D")
+#Verification of the OptPartitioning and PELT algorithms using the simplest examples
 
+devtools::install_github("lpishchagina/OptPartitioning2D")
 library(OptPartitioning2D)
 
-#Test1
+#Test1: Values of mean is constant over the interval (0,T1_n).
 T1_n <- 500
 T1_chp <- T1n
 T1_mu1 <- 0
@@ -18,7 +19,7 @@ PlotOptPart2D(T1_data, T1_OptPart$changements,T1_OptPart$means1, T1_OptPart$mean
 T1_PELT <- OptPart2D(T1_data[1,],T1_data[2,], T1_penalty, type="pruning")
 PlotOptPart2D(T1_data, T1_PELT$changements,T1_PELT$means1, T1_PELT$means2)
 
-#Test2
+#Test2: Data has 2 segments with values of means.
 T2_n <- 500
 T2_chp <- c(T2_n/2, T2_n)
 T2_mu1 <- c(0, 1)
@@ -34,7 +35,7 @@ PlotOptPart2D(T2_data, T2_OptPart$changepoints,T2_OptPart$means1, T2_OptPart$mea
 T2_PELT <- OptPart2D(T2_data[1,], T2_data[2,], T2_penalty, type="pruning")
 PlotOptPart2D(T2_data, T2_PELT$changepoints,T2_PELT$means1, T2_PELT$means2)
 
-#Test3
+#Test3:The data has multiple segments.
 T3_n <- 500
 T3_chp <- seq(from=100, to=T3_n,by=100)
 T3_mu1 <- c(1:5)
@@ -50,11 +51,11 @@ PlotOptPart2D(T3_data, T3_OptPart$changepoints,T3_OptPart$means1, T3_OptPart$mea
 T3_PELT <- OptPart2D(T3_data[1,], T3_data[2,], T3_penalty, type="pruning")
 PlotOptPart2D(T3_data, T3_PELT$changepoints,T3_PELT$means1, T3_PELT$means2)
 
-#Test4
+#Test4: Data1 and data2 have different number of segments.
 T4_n <- 500
-T4_chp <- seq(from = 100, to = T3_n, by = 100)
+T4_chp <- seq(from = 100, to = T4_n, by = 100)
 T4_mu1 <- c(1:5)
-T4_mu2 <- c(0, 0, 0, 1, 0)
+T4_mu2 <- c(0, 0, 5, 1, 0)
 T4_sigma <- 1
 T4_penalty <- 2 * T4_sigma * log(T4_n)
 
@@ -66,4 +67,3 @@ PlotOptPart2D(T4_data, T4_OptPart$changepoints,T4_OptPart$means1, T4_OptPart$mea
 T4_PELT <- OptPart2D(T4_data[1,], T4_data[2,], T4_penalty, type="pruning")
 PlotOptPart2D(T4_data, T4_PELT$changepoints,T4_PELT$means1, T4_PELT$means2)
 
-#Test5
